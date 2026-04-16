bc89a057fc0e17a6a1ccf97cdcfbcda3935e09cc
JesangIwahang <np03cs4s250062@heraldcollege.edu.np>
2026-04-17 00:31:47 +0545

    Reset branch: remove all files for fresh start

--- includes/footer.php
+++
@@ -1,6 +0,0 @@
-<?php $basePath = isset($isSubFolder) && $isSubFolder ? '../' : ''; ?>
-<!-- Ensure scripts are loaded for pages that require interaction -->
-<script src="<?php echo $basePath; ?>assets/js/script.js"></script>
-</body>
-
-</html>
